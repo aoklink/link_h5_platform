362667cb61e70941ea228b62114a429c6eb07aa0
linwanting <linwanting@linkfeeling.cn>
2019-10-12 17:45:53 +0800

    统一tools.js 请求地址

--- src/nets/tools.js
+++ src/nets/tools.js
@@ -1,23 +1,15 @@
 import axios from 'axios';
-var address = 'http://test.linkfeeling.cn/api/platform/link/';
+import * as api from './api';
 export default {
     install (Vue) {
-        // process.env.NODE_ENV !== 'production'
         Vue.prototype.client = {
-            request: function (data, method) {
+            request: function (data, method) {  
                 return axios
                     .post(
-                        address + method,
+                        api.PREFIX + '/api/platform/link/' + method,
                         JSON.stringify(data),
                         { headers: { 'Content-Type': 'application/json' } }
                     );
-                // .then(res => {
-                //     var data = res.data.data;
-                //     console.log('akak', data);
-                // })
-                // .catch(res => {
-                //     console.log(res);
-                // });
             }
         };
     }
